c767bf369c3305884b9fd08c332ba290fbe53f8a
Coder_Lucy <luciejs@gmail.com>
2022-12-29 23:06:26 -0500

    project 7

---
+++ red_black_tree/0-rb_tree_node.c
@@ -0,0 +1,24 @@
+#include "rb_trees.h"
+
+
+/**
+ * rb_tree_node -  Creates a Red-Black Tree node
+ * @parent: A pointer to the parent node of the node to create
+ * @value: The value to put in the new node
+ * @color: The color of the node
+ * Return: A pointer to the new node, or NULL on failure
+ */
+rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color)
+{
+	rb_tree_t *node;
+
+	node = malloc(sizeof(rb_tree_t));
+	if (node == NULL)
+		return (NULL);
+	node->n = value;
+	node->color = color;
+	node->parent = parent;
+	node->left = NULL;
+	node->right = NULL;
+	return (node);
+}
